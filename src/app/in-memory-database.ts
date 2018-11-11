cbb52270fbc1840b8065ef76cb227245923f20de
Igor Rocha <igortice@gmail.com>
2018-11-11 17:06:00 -0300

    init in memory database withh model category

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+import { Category } from './pages/categories/shared/category.model';
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categories: Category[] = [
+      { id: 1, name: 'Windstorm', description: 'this is a description 1' },
+      { id: 2, name: 'Bombasto', description: 'this is a description 2' },
+      { id: 3, name: 'Magneta', description: 'this is a description 3' },
+      { id: 4, name: 'Tornado', description: 'this is a description 4' }
+    ];
+
+    return { categories };
+  }
+}
