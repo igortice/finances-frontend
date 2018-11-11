2b5c9b0204b30232042604798dd81e01ff700e66
Igor Rocha <igortice@gmail.com>
2018-11-11 20:52:28 -0300

    init fake delete

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../shared/category.service';
+import { Category } from '../shared/category.model';
+import { element } from 'protractor';
 
 @Component({
   selector:    'app-category-list',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls:   [ './category-list.component.scss' ]
 })
 export class CategoryListComponent implements OnInit {
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar lista')
+    );
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm(`Deseja deletar item ${category.id}?`);
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        _ => this.categories = this.categories.filter(element => element !== category),
+        _ => alert('error ao excluir')
+      );
+    }
   }
 
 }
