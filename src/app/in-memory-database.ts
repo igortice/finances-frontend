8daeab3bc8cc189444ac34bc138eb8476ab03876
Igor Rocha <igortice@gmail.com>
2018-11-17 14:25:55 -0300

    init entries list

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,15 +1,65 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Category } from './pages/categories/shared/category.model';
 
+import { Entry } from './pages/entries/shared/entry.model';
+
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
     const categories: Category[] = [
-      { id: 1, name: 'Windstorm', description: 'this is a description 1' },
-      { id: 2, name: 'Bombasto', description: 'this is a description 2' },
-      { id: 3, name: 'Magneta', description: 'this is a description 3' },
-      { id: 4, name: 'Tornado', description: 'this is a description 4' }
+      { id: 1, name: 'Moradia', description: 'Pagamentos conta da casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de saúde e remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, Parque, Praia, etc' },
+      { id: 4, name: 'Salário', description: 'Recebimento de salário' },
+      { id: 5, name: 'Freelas', description: 'Trabalho como frelancer' }
     ];
 
-    return { categories };
+    const entries: Entry[] = [
+      {
+        id:          1,
+        name:        'Gás de Cozinha',
+        categoryId:  categories[ 0 ].id,
+        category:    categories[ 0 ],
+        paid:        true,
+        date:        '14/10/2018',
+        amount:      '70,00',
+        type:        'expense',
+        description: 'apenas uma descrição'
+      } as Entry,
+      {
+        id:          2,
+        name:        'Plano Saúde',
+        categoryId:  categories[ 1 ].id,
+        category:    categories[ 1 ],
+        paid:        true,
+        date:        '16/10/2018',
+        amount:      '430,00',
+        type:        'expense',
+        description: 'plano multiplan unimed fortaleza'
+      } as Entry,
+      {
+        id:          3,
+        name:        'Croco Beach',
+        categoryId:  categories[ 2 ].id,
+        category:    categories[ 2 ],
+        paid:        true,
+        date:        '16/10/2018',
+        amount:      '110,00',
+        type:        'expense',
+        description: 'feriado na praia com família'
+      } as Entry,
+      {
+        id:          4,
+        name:        'Recebimento 13 + Salario',
+        categoryId:  categories[ 3 ].id,
+        category:    categories[ 3 ],
+        paid:        true,
+        date:        '05/10/2018',
+        amount:      '7700,00',
+        type:        'renevue',
+        description: 'recebimento proventos'
+      } as Entry
+    ];
+
+    return { categories, entries };
   }
 }
