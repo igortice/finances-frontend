a76c6752c4b566909096693c28e2e0dbfdcb26b5
Igor Rocha <igortice@gmail.com>
2018-11-17 13:27:12 -0300

    init module entry

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,24 @@
+import { Category } from '../../categories/shared/category.model';
+
+export class Entry {
+  static types = {
+    expense: 'Despesa',
+    renevue: 'Receita'
+  };
+
+  constructor(
+    public id?: number,
+    public name?: string,
+    public description?: string,
+    public type?: string,
+    public amount?: string,
+    public date?: string,
+    public paid?: boolean,
+    public categoryId?: number,
+    public category?: Category
+  ) {}
+
+  get paidText(): string {
+    return this.paid ? 'Pago' : 'Pendente';
+  }
+}
