32fb95df31f1055ff78bb4794424f4396e1a3d2c
Igor Rocha <igortice@gmail.com>
2018-11-18 20:01:37 -0300

    init base resource model

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,6 +1,7 @@
 import { Category } from '../../categories/shared/category.model';
+import { BaseResourceModel } from '../../../shared/models/base-resource.model';
 
-export class Entry {
+export class Entry extends BaseResourceModel {
   static types = {
     expense: 'Despesa',
     revenue: 'Receita'
@@ -16,7 +17,9 @@ export class Entry {
     public paid?: boolean,
     public categoryId?: number,
     public category?: Category
-  ) {}
+  ) {
+    super();
+  }
 
   get paidText(): string {
     return this.paid ? 'Pago' : 'Pendente';
